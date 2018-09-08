a46b476afbf532b6eeee082590712f48122d94ab
baibhav73 <35353965+baibhav73@users.noreply.github.com>
2018-09-08 02:17:47 -0500

    Update install-openshift.sh

--- install-openshift.sh
+++ install-openshift.sh
@@ -8,7 +8,7 @@ export PVS=${INTERACTIVE:="true"}
 export DOMAIN=${DOMAIN:="$(curl -s ipinfo.io/ip).nip.io"}
 export USERNAME=${USERNAME:="$(whoami)"}
 export PASSWORD=${PASSWORD:=password}
-export VERSION=${VERSION:="3.10"}
+export VERSION=${VERSION:="3.9.0"}
 export SCRIPT_REPO=${SCRIPT_REPO:="https://raw.githubusercontent.com/gshipley/installcentos/master"}
 export IP=${IP:="$(ip route get 8.8.8.8 | awk '{print $NF; exit}')"}
 export API_PORT=${API_PORT:="8443"}
@@ -85,7 +85,7 @@ yum -y --enablerepo=epel install ansible pyOpenSSL
 
 [ ! -d openshift-ansible ] && git clone https://github.com/openshift/openshift-ansible.git
 
-cd openshift-ansible && git fetch && git checkout release-3.10 && cd ..
+cd openshift-ansible && git fetch && git checkout release-3.9 && cd ..
 
 cat <<EOD > /etc/hosts
 127.0.0.1   localhost localhost.localdomain localhost4 localhost4.localdomain4 
@@ -93,85 +93,21 @@ cat <<EOD > /etc/hosts
 ${IP}		$(hostname) console console.${DOMAIN}  
 EOD
 
-if [ -z $DISK ]; then 
-	echo "Not setting the Docker storage."
-else
-	cp /etc/sysconfig/docker-storage-setup /etc/sysconfig/docker-storage-setup.bk
-
-	echo DEVS=$DISK > /etc/sysconfig/docker-storage-setup
-	echo VG=DOCKER >> /etc/sysconfig/docker-storage-setup
-	echo SETUP_LVM_THIN_POOL=yes >> /etc/sysconfig/docker-storage-setup
-	echo DATA_SIZE="100%FREE" >> /etc/sysconfig/docker-storage-setup
-
-	systemctl stop docker
-
-	rm -rf /var/lib/docker
-	wipefs --all $DISK
-	docker-storage-setup
-fi
-
-systemctl restart docker
-systemctl enable docker
-
 if [ ! -f ~/.ssh/id_rsa ]; then
 	ssh-keygen -q -f ~/.ssh/id_rsa -N ""
 	cat ~/.ssh/id_rsa.pub >> ~/.ssh/authorized_keys
 	ssh -o StrictHostKeyChecking=no root@$IP "pwd" < /dev/null
 fi
 
-export METRICS="True"
-export LOGGING="True"
-
-memory=$(cat /proc/meminfo | grep MemTotal | sed "s/MemTotal:[ ]*\([0-9]*\) kB/\1/")
-
-if [ "$memory" -lt "4194304" ]; then
-	export METRICS="False"
-fi
-
-if [ "$memory" -lt "8388608" ]; then
-	export LOGGING="False"
-fi
 
 curl -o inventory.download $SCRIPT_REPO/inventory.ini
 envsubst < inventory.download > inventory.ini
-
-# add proxy in inventory.ini if proxy variables are set
-if [ ! -z "${HTTPS_PROXY:-${https_proxy:-${HTTP_PROXY:-${http_proxy}}}}" ]; then
-	echo >> inventory.ini
-	echo "openshift_http_proxy=\"${HTTP_PROXY:-${http_proxy:-${HTTPS_PROXY:-${https_proxy}}}}\"" >> inventory.ini
-	echo "openshift_https_proxy=\"${HTTPS_PROXY:-${https_proxy:-${HTTP_PROXY:-${http_proxy}}}}\"" >> inventory.ini
-	if [ ! -z "${NO_PROXY:-${no_proxy}}" ]; then
-		__no_proxy="${NO_PROXY:-${no_proxy}},${IP},.${DOMAIN}"
-	else
-		__no_proxy="${IP},.${DOMAIN}"
-	fi
-	echo "openshift_no_proxy=\"${__no_proxy}\"" >> inventory.ini
-fi
-
-mkdir -p /etc/origin/master/
-touch /etc/origin/master/htpasswd
 
 ansible-playbook -i inventory.ini openshift-ansible/playbooks/prerequisites.yml
 ansible-playbook -i inventory.ini openshift-ansible/playbooks/deploy_cluster.yml
 
 htpasswd -b /etc/origin/master/htpasswd ${USERNAME} ${PASSWORD}
 oc adm policy add-cluster-role-to-user cluster-admin ${USERNAME}
-
-if [ "$PVS" = "true" ]; then
-	for i in `seq 1 200`;
-	do
-		DIRNAME="vol$i"
-		mkdir -p /mnt/data/$DIRNAME 
-		chcon -Rt svirt_sandbox_file_t /mnt/data/$DIRNAME
-		chmod 777 /mnt/data/$DIRNAME
-		
-		sed "s/name: vol/name: vol$i/g" vol.yaml > oc_vol.yaml
-		sed -i "s/path: \/mnt\/data\/vol/path: \/mnt\/data\/vol$i/g" oc_vol.yaml
-		oc create -f oc_vol.yaml
-		echo "created volume $i"
-	done
-	rm oc_vol.yaml
-fi
 
 echo "******"
 echo "* Your console is https://console.$DOMAIN:$API_PORT"
